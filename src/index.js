6e7c8228727b49bccbe966a5c77c28748a9f26d3
Roman Andreev <anDevRom@gmail.com>
2020-10-18 13:13:28 +0300

    feat: complete morse-code task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,33 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let arr = []
+    for (let i = 0; i < expr.length; i = i + 10) {
+      arr.push(String(+expr.slice(i, i + 10)))
+    }
+    arr.forEach((el, index, array) => {
+      let arrInner = []
+      for (let i = 0; i < el.length; i = i + 2) {
+        arrInner.push(el.slice(i, i + 2))
+      }
+      arrInner.forEach((elem, ind) => {
+        switch (elem) {
+          case "10":
+            arrInner[ind] = "."
+            break
+          case "11":
+            arrInner[ind] = "-"
+            break
+        }
+      })
+      array[index] = arrInner.join("")
+    })
+    return arr.reduce((prev, curr) => {
+      if (curr === "NaN") {
+        return prev + " "
+      }
+      return prev + MORSE_TABLE[curr]
+    }, "")
 }
 
 module.exports = {
